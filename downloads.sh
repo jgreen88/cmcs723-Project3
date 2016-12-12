b2cabdefb5a67317e0c6d929ea8e10c80bbe92ce
sri-kankanahalli <sriharik@terpmail.umd.edu>
2016-12-12 04:38:05 -0500

    Testing out word alignment

--- downloads.sh
+++ downloads.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
-curl -Ss http://ttic.uchicago.edu/~wieting/paragram-phrase-XXL.zip > file.zip && \
-unzip file.zip && \
-rm file.zip
 
-curl -Ss https://doc-08-3k-docs.googleusercontent.com/docs/securesc/bj1odvti56n9sbh18h25t4deksvrrk7u/v2n9ecorfe3phc7n9e02nt38rlf3dm53/1481385600000/06848720943842814915/03078941804631754256/0B7XkCwpI5KDYNlNUTTlSS21pQmM?e=download&h=04969168175474109593&nonce=mc652j3i9p2oo&user=03078941804631754256&hash=ev63pk1juf595lvsbie49659g10kli3p > file.gz && \
+
+#curl -Ss http://ttic.uchicago.edu/~wieting/paragram-phrase-XXL.zip > file.zip && \
+#unzip file.zip && \
+#rm file.zip
+
+#curl https://doc-14-7c-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/2nm99vt11k34ap16itmgj13uvsg41cml/1481515200000/14570458592396221227/*/0B9w48e1rj-MOck1fRGxaZW1LU2M?e=download > file.zip && \
+#unzip file.zip && \
+#rm file.zip
+#mv paragram_300_sl999/* .
+#rm README.txt
+#rm file.gz
+#rm -rf paragram_300_sl999
+
+curl -Ss https://doc-0g-8s-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/2efhm13d7m7phi0o5q9jorb9l2kk1cof/1481515200000/06848720943842814915/*/0B7XkCwpI5KDYNlNUTTlSS21pQmM?e=download > file.gz && \
 gunzip file.gz && \
 rm file.gz
