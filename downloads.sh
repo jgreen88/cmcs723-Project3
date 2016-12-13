535218779b2dd6739d3c8ea4eb145b08851a9ce3
sri-kankanahalli <sriharik@terpmail.umd.edu>
2016-12-12 23:11:05 -0500

    New downloads.sh

--- downloads.sh
+++ downloads.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
 
-
-
-#curl -Ss http://ttic.uchicago.edu/~wieting/paragram-phrase-XXL.zip > file.zip && \
-#unzip file.zip && \
+#curl -Ss http://ttic.uchicago.edu/~wieting/paragram-phrase-XXL.zip > file.zip
+#unzip file.zip
 #rm file.zip
 
-#curl https://doc-14-7c-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/2nm99vt11k34ap16itmgj13uvsg41cml/1481515200000/14570458592396221227/*/0B9w48e1rj-MOck1fRGxaZW1LU2M?e=download > file.zip && \
-#unzip file.zip && \
-#rm file.zip
-#mv paragram_300_sl999/* .
-#rm README.txt
-#rm file.gz
-#rm -rf paragram_300_sl999
-
-curl -Ss https://doc-0g-8s-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/2efhm13d7m7phi0o5q9jorb9l2kk1cof/1481515200000/06848720943842814915/*/0B7XkCwpI5KDYNlNUTTlSS21pQmM?e=download > file.gz && \
-gunzip file.gz && \
+curl https://doc-14-7c-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/3l68emgbv4jvpr4o6ghm5kllbpigtrog/1481601600000/14570458592396221227/*/0B9w48e1rj-MOck1fRGxaZW1LU2M?e=download > file.zip
+unzip file.zip
+rm file.zip
+mv paragram_300_sl999/* .
+rm README.txt
 rm file.gz
+rm -rf paragram_300_sl999
+
